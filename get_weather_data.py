33c1e0d68d407db24c4377d3f110f14922001dfc
LarryWaterhouse <109979285+LarryWaterhouse@users.noreply.github.com>
2023-02-07 16:42:33 -0500

    Update get_weather_data.py

--- get_weather_data.py
+++ get_weather_data.py
@@ -1,5 +1,39 @@
-geo_df = pd.read_csv('LOAD DATA')
+# load csv of Game Data via CFBD
+import numpy as np
+import pandas as pd
+import cfbd
+from __future__ import print_function
+import time
+from cfbd.rest import ApiException
+from pprint import pprint
 
+
+configuration = cfbd.Configuration()
+configuration.api_key['Authorization'] = 'API KEY'
+configuration.api_key_prefix['Authorization'] = 'Bearer'
+api_config = cfbd.ApiClient(configuration)
+
+#get game results
+response = games = cfbd.GamesApi(api_config).get_games(year=year, season_type='both', division='fbs')
+
+game_data = pd.DataFrame(response, columns=['team', 'id', 'season','week', 'start_date',	'date', 'time',	'neutral_site','venue',	'home_team','home_points'.	'away_team',	'away_points'])
+
+# create an instance of the API class
+api_instance = cfbd.TeamsApi(cfbd.ApiClient(configuration))
+year = 56 # int | Year filter (optional)
+
+try:
+    # FBS team list
+    api_response = api_instance.get_fbs_teams(year=year)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling TeamsApi->get_fbs_teams: %s\n" % e)
+    
+geo_df = pd.DataFrame(api_response)
+
+game_data =  pd.merge(game_data,geo_df,left_index=True, right_index=True)
+
+#Function to get the weather using Open Meteo's API
 def get_weather_results(lat, long, date):
     url ='https://archive-api.open-meteo.com/v1/archive?latitude=' + str(lat) + '&longitude=' + str(long) + '&start_date=' + str(date) + '&end_date=' + str(date) + '&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m,winddirection_10m&timezone=America%2FNew_York&temperature_unit=fahrenheit&windspeed_unit=ms&precipitation_unit=inch'
     r = requests.get(url)
@@ -23,6 +57,8 @@ def get_weather_results(lat, long, date):
 get_data = get_weather_results(lat,long,date)
 
 results = []
+#Loop through Game_data for the Lat, Long, Date
+
 for x in range(0, len(game_data)):
     try:
         results.append(get_weather_results(game_data['lat'][x], game_data['long'][x], game_data['date'][x]))
@@ -30,3 +66,7 @@ for x in range(0, len(game_data)):
         continue
 
 result_df = pd.concat(results, ignore_index=True)
+
+merge_df = pd.merge(game_data,result_df,left_index=True, right_index=True)
+
+
